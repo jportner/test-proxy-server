735d0e9daf2c3b66d8dddb9a52c2c6cdcee213a7
David Keppler <dkeppler@mitre.org>
2013-10-23 17:23:19 -0400

    Update test proxy to send webrtc video info to VM
    * Part of the input event server / fbstream merge effort
    * Reorg of the debug printouts
    * Defaults to proxy's IP for STUN
    
    Change-Id: Ifa544d6caf1917194e3cc7d494c63def1baf80b1

--- src/org/mitre/svmp/TestProxy/TestProxy.java
+++ src/org/mitre/svmp/TestProxy/TestProxy.java
@@ -44,7 +44,9 @@ import org.mitre.svmp.protocol.SVMPProtocol.Request;
 import org.mitre.svmp.protocol.SVMPProtocol.Request.RequestType;
 import org.mitre.svmp.protocol.SVMPProtocol.Response;
 import org.mitre.svmp.protocol.SVMPProtocol.VideoStreamInfo;
+import org.mitre.svmp.protocol.SVMPProtocol.WebRTCMessage;
 import org.mitre.svmp.protocol.SVMPProtocol.Response.ResponseType;
+import org.mitre.svmp.protocol.SVMPProtocol.WebRTCMessage.WebRTCType;
 
 /**
  * @author David Keppler <dkeppler@mitre.org>
@@ -53,8 +55,13 @@ import org.mitre.svmp.protocol.SVMPProtocol.Response.ResponseType;
 public class TestProxy {
 
 	// Set this to the IP address of the SVMP VM.
-	public static String VM_ADDRESS = "192.168.42.100";
-    //public static String VM_ADDRESS = "127.0.0.1";
+	public static final String VM_ADDRESS = "192.168.42.100";
+    // public static String VM_ADDRESS = "127.0.0.1";
+
+	// STUN server Host:Port to use
+	// set to null to use the proxy server's IP and default port 3478
+	public static String STUN_SERVER = null;
+	// public static String STUN_SERVER = "192.168.42.120:3478";
 
 	// To use SSL:
 	// 1) use keytool to generate a self signed cert in a new keystore
@@ -67,21 +74,21 @@ public class TestProxy {
 
 	public static final int LISTEN_PORT = 8002;
 
-//	private static final int UNAUTHENTICATED = 0;
-//	private static final int AUTHENTICATED = 1;
-//	private static final int PROXYING = 2;
-//	private static final int VIDEOINFO = 3;
+	private static final boolean DEBUG = true;
+
+	// private static final int UNAUTHENTICATED = 0;
+	// private static final int AUTHENTICATED = 1;
+	// private static final int PROXYING = 2;
+	// private static final int VIDEOINFO = 3;
 	private enum State {
-	    UNAUTHENTICATED,
-	    AUTHENTICATED,
-        VIDEOINFO,
-	    PROXYING,
+	    UNAUTHENTICATED, AUTHENTICATED, VIDEOINFO, PROXYING,
 	}
 
 	private Socket inputService = null;
 	private InputStream  inputServiceIn = null;
 	private OutputStream inputServiceOut = null;
 	private Thread inputServiceThread;
+
     private boolean sessionTimedOut;
     private AuthResponseType sessionTimedOutType;
     private Date lastUpdated;
@@ -111,6 +118,13 @@ public class TestProxy {
 			try {
 				Socket s = daemon.accept();
 				System.out.println("Client connection received from " + s.getInetAddress());
+
+				// use local IP of the socket if no STUN server set
+				if (STUN_SERVER == null) {
+					STUN_SERVER = "" + s.getLocalAddress().getHostAddress()
+							+ ":3478";
+				}
+
 				session = new TestProxy(s);
 				session.run();
 			} catch (Exception e) {
@@ -220,12 +234,14 @@ public class TestProxy {
                 SessionHandler.closeSession(sessionToken);
 
 				System.out.println("Client disconnected, ending thread");
+				// sendWebRTCBye();
 				cleanup();
 				return;
 			}
 
-			System.out.println("Request received: " + req.getType().name() + ", State = " + state.name());
-			
+			// do debug printing of the received message
+			debugUpstream(req);
+
 			Response.Builder response = SVMPProtocol.Response.newBuilder();
 			switch (state) {
 			case UNAUTHENTICATED:
@@ -245,10 +261,11 @@ public class TestProxy {
 						response.build().writeDelimitedTo(out);
 						System.out.println("AUTH_OK sent");
 					}
+
+					// connect to VM, send it the video params, and wait for VMREADY
 					connectToVM(out);
+
 					response.clear();
-					System.out.println("Waiting for \"VM to start\"");
-					Thread.sleep(2000); // pretend we have to wait a bit for the VM to connect
 					response.setType(ResponseType.VMREADY);
 					response.setMessage(VM_ADDRESS);
                     state = State.VIDEOINFO;
@@ -270,9 +287,11 @@ public class TestProxy {
 			case AUTHENTICATED:
 				break;
 			case VIDEOINFO:
-			    if (req.getType() == RequestType.VIDEO_PARAMS) {
-			        makeVideoChannelInfo().writeDelimitedTo(out);
-			        state = State.PROXYING;
+				if (req.getType() == RequestType.VIDEO_PARAMS) {
+					Response.newBuilder().setType(ResponseType.VIDSTREAMINFO)
+							.setVideoInfo(makeVideoChannelInfo()).build()
+							.writeDelimitedTo(out);
+					state = State.PROXYING;
 			    } else {
 			        // unexpected message type, ignore
 			    }
@@ -283,102 +302,9 @@ public class TestProxy {
                         // screen was touched, renew lastUpdated time to extend session idle timeout and reuse limit
                         lastUpdated = new Date();
                         break;
+				default:
+					break;
                 }
-// Debug printouts
-/*
-				switch(req.getType()) {
-				case SCREENINFO:
-					response.clear();
-					ScreenInfo.Builder scr = SVMPProtocol.ScreenInfo.newBuilder();
-					scr.setX(360);
-					scr.setY(480);
-					response.setType(ResponseType.SCREENINFO);
-					response.setScreenInfo(scr);
-					response.build().writeDelimitedTo(out);
-					break;
-				case SENSOREVENT:
-					if (req.hasSensor()) {
-						System.out.print("Sensor type = " + req.getSensor().getType().name());
-						System.out.print(", Accuracy = " + req.getSensor().getAccuracy());
-						System.out.print(", Timestamp = " + req.getSensor().getTimestamp());
-						System.out.print(", Values = [");
-						int ctr = 0;
-						for (float v : req.getSensor().getValuesList()) {
-							if( ctr++ > 0 )
-								System.out.print(", ");
-							System.out.print(v);
-						}
-						System.out.println("]");
-					}
-					break;
-				case TOUCHEVENT:
-					if (req.hasTouch()) {
-						System.out.println("Action = " + req.getTouch().getAction());
-						for (SVMPProtocol.TouchEvent.PointerCoords p : req.getTouch().getItemsList()) {
-							System.out.println("	id = " + p.getId() + " ; x = " + p.getX() + " ; y = " + p.getY());
-						}
-					}
-					break;
-				case INTENT:
-					break;
-				case LOCATION:
-					if( req.hasLocationRequest() ) {
-						SVMPProtocol.LocationRequest lr = req.getLocationRequest();
-						SVMPProtocol.LocationRequest.LocationRequestType type = lr.getType();
-						String message = "";
-						switch( type ) {
-							case PROVIDERINFO:
-								SVMPProtocol.LocationProviderInfo lpi = lr.getProviderInfo();
-								message = String.format("Location provider info: [provider '%s', reqNetwork '%b', " +
-										"reqSat '%b', reqCell '%b', hasMonCost '%b', suppAlt '%b', suppSpeed '%b', " +
-										"suppBearing '%b', powerReq '%d', accuracy '%d']",
-										lpi.getProvider(),
-										lpi.getRequiresNetwork(),
-										lpi.getRequiresSatellite(),
-										lpi.getRequiresCell(),
-										lpi.getHasMonetaryCost(),
-										lpi.getSupportsAltitude(),
-										lpi.getSupportsSpeed(),
-										lpi.getSupportsBearing(),
-										lpi.getPowerRequirement(),
-										lpi.getAccuracy() );
-								break;
-							case PROVIDERSTATUS:
-								SVMPProtocol.LocationProviderStatus lps = lr.getProviderStatus();
-								message = String.format("Location provider enabled: [provider '%s', status '%d']",
-										lps.getProvider(),
-										lps.getStatus() );
-								break;
-							case PROVIDERENABLED:
-								SVMPProtocol.LocationProviderEnabled lpe = lr.getProviderEnabled();
-								message = String.format("Location provider enabled: [provider '%s', enabled '%b']",
-										lpe.getProvider(),
-										lpe.getEnabled() );
-								break;
-							case LOCATIONUPDATE:
-								SVMPProtocol.LocationUpdate lu = lr.getUpdate();
-								message = String.format("Location update: [provider '%s', lat '%.2f', lon '%.2f', time '%s']",
-										lu.getProvider(),
-										lu.getLatitude(),
-										lu.getLongitude(),
-										new Date(lu.getTime()).toString() );
-								break;
-						}
-						System.out.println(message);
-					}
-					break;
-                case ROTATION_INFO:
-                    int rotation = req.getRotationInfo().getRotation();
-                    System.out.println("Rotation info: [rotation '" + rotation + "']");
-                    break;
-				case WEBRTC:
-				    System.out.println(req.getWebrtcMsg().getJson());
-				    break;
-				}
-				System.out.println("Request forwarded to input server");
-*/
-// End debug printouts
-
 
 				req.writeDelimitedTo(inputServiceOut);
 				break;	// PROXYING state
@@ -386,34 +312,53 @@ public class TestProxy {
 		}
 	}
 
+	private void sendWebRTCBye() throws IOException {
+		Request.newBuilder()
+				.setType(RequestType.WEBRTC)
+				.setWebrtcMsg(
+						WebRTCMessage.newBuilder().setType(WebRTCType.BYE))
+				.build().writeDelimitedTo(inputServiceOut);
+	}
+
 	private void connectToVM(OutputStream client) throws UnknownHostException, IOException {
 		System.out.println("Connecting to Input service daemon");
 		inputService = new Socket(VM_ADDRESS, INPUT_SERVICE_PORT);
 		inputServiceOut = inputService.getOutputStream();
 		inputServiceIn  = inputService.getInputStream();
-		inputServiceThread = new InputServiceResponseHandler(client, inputServiceIn);
+
+		System.out.println("Sending VIDEO_PARAMS to VM");
+		Request.newBuilder().setType(RequestType.VIDEO_PARAMS)
+				.setVideoInfo(makeVideoChannelInfo()).build()
+				.writeDelimitedTo(inputServiceOut);
+
+		System.out.println("Waiting for VM to be ready");
+		Response resp = Response.parseDelimitedFrom(inputServiceIn);
+		if (resp.getType() != ResponseType.VMREADY)
+			throw new IOException("Expecting VMREADY from VM, but got: " + resp.getType().name());
+
 		System.out.println("Input service daemon connected. Starting listen thread.");
+		inputServiceThread = new InputServiceResponseHandler(client, inputServiceIn);
 		inputServiceThread.start();
 	}
 	
-	private Response makeVideoChannelInfo() {
-        //final String iceServers = "{ \"iceServers\" : [ { \"url\" : \"stun:128.29.199.74.3478\" } ] }";
-        final String iceServers = "{ \"iceServers\" : [ { \"url\" : \"stun:192.168.42.152:3478\" } ] }";
-	    final String videoConstraints = "{ \"audio\" : true , \"video\" : { \"mandatory\" : {}, \"optional\" : []} }";
-	    final String pcConstraints = "{ \"optional\" : [ { \"DtlsSrtpKeyAgreement\" : true } ] }";
-	    
-	    VideoStreamInfo.Builder vidInfo = VideoStreamInfo.newBuilder();
-	    vidInfo.setIceServers(iceServers);
-	    vidInfo.setPcConstraints(pcConstraints);
-	    vidInfo.setVideoConstraints(videoConstraints);
-	    
-	    return Response.newBuilder()
-	        .setType(ResponseType.VIDSTREAMINFO)
-	        .setVideoInfo(vidInfo)
-	        .build();
+	private VideoStreamInfo makeVideoChannelInfo() {
+		final String iceServers = "{ \"iceServers\" : [ { \"url\" : \"stun:"
+				+ STUN_SERVER + "\" } ] }";
+		final String videoConstraints = "{ \"audio\" : true , \"video\" : { \"mandatory\" : {}, \"optional\" : []} }";
+		final String pcConstraints = "{ \"optional\" : [ { \"DtlsSrtpKeyAgreement\" : true } ] }";
+
+		VideoStreamInfo.Builder vidInfo = VideoStreamInfo.newBuilder();
+		vidInfo.setIceServers(iceServers);
+		vidInfo.setPcConstraints(pcConstraints);
+		vidInfo.setVideoConstraints(videoConstraints);
+
+//		return Response.newBuilder().setType(ResponseType.VIDSTREAMINFO)
+//				.setVideoInfo(vidInfo).build();
+
+		return vidInfo.build();
 	}
 
-	public void cleanup() {
+    public void cleanup() {
         // close the connection to the client
         try {
             if (session != null && !session.isClosed())
@@ -423,12 +368,14 @@ public class TestProxy {
         }
         // close the connection to the VM
         try {
+		if (inputServiceThread != null)
+			inputServiceThread.stop();
 	        if (inputService != null && !inputService.isClosed())
 	            inputService.close();
         } catch (IOException e) {
             // do nothing
         }
-	}
+    }
 		
 	private class InputServiceResponseHandler extends Thread {
 		private OutputStream toClient;
@@ -445,33 +392,9 @@ public class TestProxy {
 				while (true) {
 					Response r = Response.parseDelimitedFrom(fromService);
 					System.out.println("Sending response back to client: " + r.getType().name());
-/*
-// Debug printouts
-					if( r.getType() == ResponseType.LOCATION ) {
-						SVMPProtocol.LocationResponse lr = r.getLocationResponse();
-						String message = "";
-						switch( lr.getType() ) {
-							case SUBSCRIBE:
-								SVMPProtocol.LocationSubscribe ls = lr.getSubscribe();
-								message = String.format("Location subscribe: [type '%s', provider '%s', minTime '%d', "
-										+ "minDist '%.2f']",
-										ls.getType().name(),
-										ls.getProvider(),
-										ls.getMinTime(),
-										ls.getMinDistance() );
-								break;
-							case UNSUBSCRIBE:
-								SVMPProtocol.LocationUnsubscribe lu = lr.getUnsubscribe();
-								message = String.format("Location unsubscribe: [provider '%s']", lu.getProvider() );
-								break;
-						}
-						System.out.println(message);
-					}
-					if (r.getType() == ResponseType.WEBRTC) {
-					    System.out.println(r.getWebrtcMsg().getJson());
-					}
-// End debug printouts
-*/
+
+					debugDownstream(r);
+
 					synchronized (toClient) {
 						r.writeDelimitedTo(toClient);
 					}
@@ -480,6 +403,143 @@ public class TestProxy {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+	}
+
+	// Debug print outs for incoming messages from the client
+	private void debugUpstream(Request req) {
+		if (!DEBUG)
+			return;
+
+		// System.out.println("Request received: " + req.getType().name());
+		// System.out.println("State = " + state.name());
+
+		switch (req.getType()) {
+		case SCREENINFO:
+			break;
+		case SENSOREVENT:
+//			if (req.hasSensor()) {
+//				System.out.print("Sensor type = "
+//						+ req.getSensor().getType().name());
+//				System.out.print(", Accuracy = "
+//						+ req.getSensor().getAccuracy());
+//				System.out.print(", Timestamp = "
+//						+ req.getSensor().getTimestamp());
+//				System.out.print(", Values = [");
+//				int ctr = 0;
+//				for (float v : req.getSensor().getValuesList()) {
+//					if (ctr++ > 0)
+//						System.out.print(", ");
+//					System.out.print(v);
+//				}
+//				System.out.println("]");
+//			}
+			break;
+		case TOUCHEVENT:
+//			if (req.hasTouch()) {
+//				System.out.println("Action = " + req.getTouch().getAction());
+//				for (SVMPProtocol.TouchEvent.PointerCoords p : req.getTouch()
+//						.getItemsList()) {
+//					System.out.println("    id = " + p.getId() + " ; x = "
+//							+ p.getX() + " ; y = " + p.getY());
+//				}
+//			}
+			break;
+		case INTENT:
+			break;
+//		case LOCATION:
+//			if (req.hasLocationRequest()) {
+//				SVMPProtocol.LocationRequest lr = req.getLocationRequest();
+//				SVMPProtocol.LocationRequest.LocationRequestType type = lr
+//						.getType();
+//				String message = "";
+//				switch (type) {
+//				case PROVIDERINFO:
+//					SVMPProtocol.LocationProviderInfo lpi = lr
+//							.getProviderInfo();
+//					message = String
+//							.format("Location provider info: [provider '%s', reqNetwork '%b', "
+//									+ "reqSat '%b', reqCell '%b', hasMonCost '%b', suppAlt '%b', suppSpeed '%b', "
+//									+ "suppBearing '%b', powerReq '%d', accuracy '%d']",
+//									lpi.getProvider(),
+//									lpi.getRequiresNetwork(),
+//									lpi.getRequiresSatellite(),
+//									lpi.getRequiresCell(),
+//									lpi.getHasMonetaryCost(),
+//									lpi.getSupportsAltitude(),
+//									lpi.getSupportsSpeed(),
+//									lpi.getSupportsBearing(),
+//									lpi.getPowerRequirement(),
+//									lpi.getAccuracy());
+//					break;
+//				case PROVIDERSTATUS:
+//					SVMPProtocol.LocationProviderStatus lps = lr
+//							.getProviderStatus();
+//					message = String
+//							.format("Location provider enabled: [provider '%s', status '%d']",
+//									lps.getProvider(), lps.getStatus());
+//					break;
+//				case PROVIDERENABLED:
+//					SVMPProtocol.LocationProviderEnabled lpe = lr
+//							.getProviderEnabled();
+//					message = String
+//							.format("Location provider enabled: [provider '%s', enabled '%b']",
+//									lpe.getProvider(), lpe.getEnabled());
+//					break;
+//				case LOCATIONUPDATE:
+//					SVMPProtocol.LocationUpdate lu = lr.getUpdate();
+//					message = String
+//							.format("Location update: [provider '%s', lat '%.2f', lon '%.2f', time '%s']",
+//									lu.getProvider(), lu.getLatitude(),
+//									lu.getLongitude(),
+//									new Date(lu.getTime()).toString());
+//					break;
+//				}
+//				System.out.println(message);
+//			}
+//			break;
+		case WEBRTC:
+			System.out.println("Request received: " + req.getType().name());
+			System.out.println(req.getWebrtcMsg().getJson());
+			break;
+		default:
+			System.out.println("Request received: " + req.getType().name());
+			// System.out.println("State = " + state.name());
+			break;
+		}
+	}
+
+	// Debug print outs for incoming messages from the VM
+	private void debugDownstream(Response r) {
+		if (!DEBUG)
+			return;
+
+		System.out.println("Sending response back to client: "
+				+ r.getType().name());
+
+//		if (r.getType() == ResponseType.LOCATION) {
+//			SVMPProtocol.LocationResponse lr = r.getLocationResponse();
+//			String message = "";
+//			switch (lr.getType()) {
+//			case SUBSCRIBE:
+//				SVMPProtocol.LocationSubscribe ls = lr.getSubscribe();
+//				message = String.format(
+//						"Location subscribe: [type '%s', provider '%s', minTime '%d', "
+//								+ "minDist '%.2f']", ls.getType().name(),
+//						ls.getProvider(), ls.getMinTime(), ls.getMinDistance());
+//				break;
+//			case UNSUBSCRIBE:
+//				SVMPProtocol.LocationUnsubscribe lu = lr.getUnsubscribe();
+//				message = String.format(
+//						"Location unsubscribe: [provider '%s']",
+//						lu.getProvider());
+//				break;
+//			}
+//			System.out.println(message);
+//		}
+
+		if (r.getType() == ResponseType.WEBRTC) {
+			System.out.println(r.getWebrtcMsg().getJson());
 		}
 	}
 }
